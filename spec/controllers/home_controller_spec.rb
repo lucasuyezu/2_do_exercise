21382607033dab2633dc7d2eacc281b8730f6ba6
Lucas Uyezu <lucas.uyezu@gmail.com>
2010-02-28 22:33:10 -0300

    Specs for login/logout passing

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe HomeController do
-
   def login(email, password)
     visit("/users/sign_in")
     fill_in 'Email', :with => email
@@ -9,15 +8,29 @@ describe HomeController do
     click 'Sign in'
   end
 
+  def logout
+    visit("/home")
+    click "Sign out"
+  end
+
+  before(:each) do
+    @user = Factory(:user)
+    @user.confirm!
+  end
+
+  it "should have a link to sign_in" do
+    visit "/"
+    page.should have_content("Sign in as User")
+  end
+
   it "should login with correct email/password" do
-    user = Factory(:user)
-    login(user.email, user.password)
-    page.should have_content("Signed in successfully")
+    login(@user.email, @user.password)
+    page.should have_content("Sign out")
+    logout()
   end
 
   it "should not login with incorrect email/password" do
-    user = Factory(:user)
-    login(user.email, user.password + "wrong")
+    login(@user.email, @user.password + "wrong")
     page.should have_content("Invalid email or password")
   end
 
